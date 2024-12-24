e87286bd1121ec9354886c57ccd90696f3a1bee0
babarqb <babarqb@gmail.com>
2024-12-24 13:45:19 +0500

    setup shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import {Button} from "@/components/ui/button";
 
 const Homepage = () => {
     return (
         <div>
             <h2 className={`text-5xl text-green-500`}>Prostore</h2>
+            <Button>Button</Button>
         </div>
     );
 };
