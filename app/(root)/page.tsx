9a87fbebb802f0746d047ba5f651481e2469312a
babarqb <babarqb@gmail.com>
2024-12-24 14:04:54 +0500

    create .env file and constants files for configuring app metadata info

---
+++ app/(root)/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const metadata = {
+    title: 'Home'
+};
+
+const Homepage = () => {
+    return (
+        <div>
+            <h2 className={``}>Prostore</h2>
+        </div>
+    );
+};
+
+export default Homepage;
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,18 @@ import type {Metadata} from "next";
 import {Inter} from "next/font/google";
 import "@/assets/styles/globals.css";
 import React from "react";
+import {APP_DESCRIPTION, APP_NAME, SERVER_URL} from "@/lib/constants";
 
 const inter = Inter({
     subsets: ["latin"],
 });
 export const metadata: Metadata = {
-    title: "Prostore",
-    description: "A Modern ecommerce platform built with Next.js",
+    title: {
+        template: `${APP_NAME} | %s`,
+        default: `${APP_NAME}`,
+    },
+    description: `${APP_DESCRIPTION}`,
+    metadataBase: new URL(SERVER_URL),
 };
 
 export default function RootLayout({children,}: Readonly<{
